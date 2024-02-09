eee2ee690b94a29079fb37fc743f782ac245ba95
uliana <43916971+UlianaShpineva@users.noreply.github.com>
2024-02-09 16:08:30 +0300

    Update lab1.py
    .

--- lab1.py
+++ lab1.py
@@ -4,6 +4,7 @@ def read_data():
     dat = []
     matrix = []
     b = []
+    x_0 = []
     inp_type = input("Выберете способ ввода данных (f/c): ")
     if inp_type == "f":
         filename = input("Введите имя файла: ")
@@ -18,6 +19,8 @@ def read_data():
         print("Введите матрицу:")
         for i in range(size):
             matrix.append(list(map(float, input().split())))
+        print("Введите начальный вектор:")
+        x_0 = list(map(float, input().split()))
     else:
         print("Некорректный ввод")
         read_data()
@@ -25,21 +28,27 @@ def read_data():
     for i in range(size):
         b.append(matrix[i].pop())
 
+    if size > 20 or size <= 1:
+        print("Задан некорректный размер")
+        read_data()
     dat.append(accuracy)
     dat.append(size)
     dat.append(matrix)
     dat.append(b)
+    dat.append(x_0)
     return dat
 
 
 class Solver:
-    def __init__(self, accuracy, size, matrix, b):
+    def __init__(self, accuracy, size, matrix, b, x_0):
         self.accuracy = accuracy
         self.size = size
         self.matrix = matrix
         self.b = b
+        self.x_0 = x_0
 
-    M: int = 100
+    M: int = 1000
+    res: list[float] = []
 
     def is_diag(self) -> bool:
         flag = False
@@ -53,13 +62,17 @@ class Solver:
     def to_diag(self) -> None:
         for i in range(self.size):
             max_in_line = max(map(abs, self.matrix[i][i:]))
+            if max_in_line != max(self.matrix[i][i:]):
+                max_in_line *= -1
             max_in_column = max(map(abs, self.get_column(i)[i:]))
+            if max_in_column != max(self.get_column(i)[i:]):
+                max_in_column *= -1
             if max_in_column >= max_in_line:
-                line = self.get_column(i)[i:].index(max_in_line) + i
+                line = self.get_column(i).index(max_in_column)
                 self.matrix[i], self.matrix[line] = self.matrix[line], self.matrix[i]
                 self.b[i], self.b[line] = self.b[line], self.b[i]
             else:
-                column = self.matrix[i].index(max_in_column)
+                column = self.matrix[i].index(max_in_line)
                 tmp_column = self.get_column(column)
                 self.set_column(column, self.get_column(i))
                 self.set_column(i, tmp_column)
@@ -80,15 +93,17 @@ class Solver:
         print()
 
     def solve(self) -> (list[float], int, list[float]):
-        d = [num / self.matrix[i][i] for i, num in enumerate(self.b)]
-        x = d.copy()
+        d = [b_i / self.matrix[i][i] for i, b_i in enumerate(self.b)]
+
+        # x = d.copy()
+        x = self.x_0.copy()
         cnt = 0
 
         while True:
             cnt += 1
             if cnt >= self.M:
                 print("Превышено максимальное количество итераций")
-                break
+                exit()
 
             x_new = x.copy()
             for i in range(self.size):
@@ -97,14 +112,20 @@ class Solver:
                 x_new[i] = (self.b[i] - s1 - s2) / self.matrix[i][i]
 
             if all(abs(x_new[i] - x[i]) <= self.accuracy for i in range(self.size)):
-                return x_new, cnt, list(abs(x_new[i] - x[i]) for i in range(self.size))
+                self.res = x_new
+                return x_new, cnt, list(abs(x_new[k] - x[k]) for k in range(self.size))
 
             x = x_new
+            print(x)
+
+    def nev(self) -> list[float]:
+        nevv = [sum(self.res[j] * self.matrix[i][j] for j in range(self.size)) - self.b[i] for i in range(self.size)]
+        return nevv
 
 
 if __name__ == '__main__':
     data = read_data()
-    solver = Solver(data[0], data[1], data[2], data[3])
+    solver = Solver(data[0], data[1], data[2], data[3], data[4])
     print("Получена матрица:")
     solver.print()
     if not solver.is_diag():
@@ -113,10 +134,17 @@ if __name__ == '__main__':
         solver.print()
         if not solver.is_diag():
             print("Приведение к матрице диагонального преобладания невозможно")
-            exit()
     x_res, cnt_res, accuracy_vect = solver.solve()
     print("Вектор неизвестных:")
-    print(x_res, "\n")
+    for i in range(len(x_res)):
+        print("x_", (i + 1), " = ", x_res[i])
+    print()
     print("Количество итераций: ", cnt_res, "\n")
     print("Вектор погрешностей:")
-    print(accuracy_vect)
+    for i in range(len(accuracy_vect)):
+        print("x_", (i + 1), " = ", accuracy_vect[i])
+    print()
+    nev = solver.nev()
+    print("Невязка: ")
+    for i in range(len(accuracy_vect)):
+        print(nev[i])
